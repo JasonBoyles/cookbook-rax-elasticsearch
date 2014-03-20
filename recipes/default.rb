87b9933872949409736a8b72167c897a71c17aef
Jason Boyles <jason@duncancreek.net>
2014-03-20 15:04:48 -0500

    comment more!

--- recipes/default.rb
+++ recipes/default.rb
@@ -16,7 +16,7 @@ include_recipe 'java::default'
 #  node[:network][:interfaces][:en2][:addresses].find \
 #  {|addr, addr_info| addr_info[:family] == "inet"}.first
 
-node.set[:elasticsearch][:custom_config] = {
-  'discovery.zen.ping.multicast.address' => cn_interface_ipv4 }
+# node.set[:elasticsearch][:custom_config] = {
+#  'discovery.zen.ping.multicast.address' => cn_interface_ipv4 }
 
 include_recipe 'elasticsearch::default'
