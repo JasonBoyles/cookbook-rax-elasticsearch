11cf566abfb4f7a391726c75ea4990f72c2a6e46
Jason Boyles <jason@duncancreek.net>
2014-03-24 11:22:25 -0500

    cluster on eth2, bind to localhost

--- recipes/default.rb
+++ recipes/default.rb
@@ -20,6 +20,8 @@ Chef::Log.info("print interface ip is #{cn_interface_ipv4}")
 
 node.set[:elasticsearch][:custom_config] = {
   'discovery.zen.ping.multicast.address' => cn_interface_ipv4,
+  'network.publish_host' => '_[eth2]:ipv4_',
+  'network.bind_host' => '127.0.0.1'
   }
 
 include_recipe 'elasticsearch::default'
